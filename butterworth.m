c5b64c5328831341aff3a3efe6ae78c9cb9457e5
Bruno Alexandre Fraga <brunoalexandrefraga@gmail.com>
2024-07-21 14:30:44 -0300

    Updating butterworth and removing asv files.

--- butterworth.m
+++ butterworth.m
@@ -1,13 +1,13 @@
-wp = 2; % [kHz]
-ws = 3.2; % [kHz]
+omega_p = 2; % [kHz]
+omega_s = 3.2; % [kHz]
 
 Amax = 0.8; % [dB]
 Amin = 50; % [dB]
 
-n_wp = wp / wp; % wp normalized
-n_ws = ws / wp; % ws normalized
+n_wp = omega_p / omega_p; % wp normalized
+n_ws = omega_s / omega_p; % ws normalized
 
-E = (10 ^ (Amax / 10) - 1) ^ (1 / 2);
+epsilon = (10 ^ (Amax / 10) - 1) ^ (1 / 2);
 
 a = 10 ^ (0.1 * Amin) - 1;
 b = 10 ^ (0.1 * Amax) - 1;
@@ -16,22 +16,23 @@ d = 2 * log10(n_ws);
 
 n = ceil(c / d);
 
-n_o = zeros(1, n);
-n_w = zeros(1, n);
-n_s = zeros(1, n);
+sigma_bar = zeros(1, n);
+omega_bar = zeros(1, n);
+s_bar = zeros(1, n);
 
 for k = 1 : n
    factor = 1;
 
-   n_o(k) = E ^ (-1 / n) * sin(((2 * k - 1) * pi) / (2 * n));
-   n_w(k) = E ^ (-1 / n) * cos(((2 * k - 1) * pi) / (2 * n));
+   sigma_bar(k) = epsilon ^ (-1 / n) * sin(((2 * k - 1) * pi) / (2 * n));
+   omega_bar(k) = epsilon ^ (-1 / n) * cos(((2 * k - 1) * pi) / (2 * n));
 
-   if n_o(k) > 0
+   if sigma_bar(k) > 0
        factor = -1;
    end
 
-   n_s(k) = factor * (n_o(k) + 1i * n_w(k));
+   s_bar(k) = factor * (sigma_bar(k) + 1i * omega_bar(k));
 end
 
+G0 = epsilon ^ (-1);
 
-G = zpk([], n_s, E ^(-1))
+T = tf([G0], s_bar)
